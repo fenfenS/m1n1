asdfugil/m1n1-idevice

cpufreq: Add functions to unrestrict boost states
This is used in conjunction with the CPU pstate latencies experiment.
It is not for general use as it puts the CPU in an untested
configuration.

Signed-off-by: Nick Chan <towinchenmi@gmail.com>

proxyclient/m1n1/proxy.py
@@ -642,6 +642,7 @@ class M1N1Proxy(Reloadable):
     P_DAPF_INIT = 0x1201
 
     P_CPUFREQ_INIT = 0x1300
+    P_CPUFREQ_UNRESTRICT_BOOST = 0x1301
 
     def __init__(self, iface, debug=False):
         self.debug = debug
@@ -1141,6 +1142,9 @@ class M1N1Proxy(Reloadable):
 
     def cpufreq_init(self):
         return self.request(self.P_CPUFREQ_INIT)
+
+    def cpufreq_unrestrict_boost(self):
+        return self.request(self.P_CPUFREQ_UNRESTRICT_BOOST)
 
 __all__.extend(k for k, v in globals().items()
                if (callable(v) or isinstance(v, type)) and v.__module__ == __name__)

fenfenS/m1n1

proxyclient/m1n1/proxy.py
@@ -643,6 +643,7 @@ class M1N1Proxy(Reloadable):
 
     P_CPUFREQ_INIT = 0x1300
     P_CPUFREQ_PREPARE_BAUD = 0x1301
+    P_CPUFREQ_UNRESTRICT_BOOST = 0x1302
 
     def __init__(self, iface, debug=False):
         self.debug = debug
@@ -1144,6 +1145,9 @@ class M1N1Proxy(Reloadable):
         return self.request(self.P_CPUFREQ_INIT)
     def cpufreq_prepare_1500000_baud(self):
         self.request(self.P_CPUFREQ_PREPARE_BAUD)
+
+    def cpufreq_unrestrict_boost(self):
+        return self.request(self.P_CPUFREQ_UNRESTRICT_BOOST)
 
 __all__.extend(k for k, v in globals().items()
                if (callable(v) or isinstance(v, type)) and v.__module__ == __name__)